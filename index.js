a691a33cbcd7912cbf05732d7839332153dd6f9f
nishadinfo09 <mdnishadali03@gmail.com>
2025-11-16 12:55:43 +0600

    add firebase admin

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 3000;
 
 //firebase admin
 const admin = require("firebase-admin");
-const serviceAccount = require("./smart-deals-application.json");
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString(
+  "utf8"
+);
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
